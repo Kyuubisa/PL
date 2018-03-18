cf15c34a3295ad30b01297668e5c60e3e9783afb
Kyuubisa <sasu.naru.kyuubi@gmail.com>
2018-03-18 20:38:26 +0300

    first commit

---
+++ PL/PL/Loading.cs
@@ -0,0 +1,64 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Android.Graphics;
+using Android.Content.PM;
+using Android.Support.V7.CardView;
+using System.Threading;
+using System.Collections.Generic;
+using Android.Support.V7.App;
+using ME.Itangqi.Waveloadingview;
+using static Android.Widget.SeekBar;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    [Activity(MainLauncher = true, Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
+    public class Loading : AppCompatActivity, IOnSeekBarChangeListener
+    {
+        WaveLoadingView waveLodingView;
+        SeekBar seekBar;
+        int progress = 0;
+
+        public void OnProgressChanged(SeekBar seekBar, int progress, bool fromUser)
+        {
+            waveLodingView.ProgressValue = progress;
+        }
+        public void OnStartTrackingTouch(SeekBar seekBar){}
+        public void OnStopTrackingTouch(SeekBar seekBar){}
+
+        protected async override void OnCreate(Bundle savedInstanceState)
+        {
+            MainActivity main = new MainActivity();
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Loading);
+            this.RequestedOrientation = ScreenOrientation.Portrait;
+            waveLodingView = FindViewById<WaveLoadingView>(Resource.Id.waveLoadingView);
+            seekBar = FindViewById<SeekBar>(Resource.Id.seekbar);
+            waveLodingView.ProgressValue = 0;
+            seekBar.SetOnSeekBarChangeListener(this);
+            await Task.Run(() =>
+            {
+                for (int i = 0; i <= 100; i++)
+                {
+                    if (progress < 100)
+                    {
+                        progress++;
+                        seekBar.Progress = progress;
+                    }
+
+                    Thread.Sleep(100);
+
+                    if (progress == 100)
+                    {
+                        seekBar.Progress = progress;
+                        Intent cs = new Intent(this, typeof(MainActivity));
+                        StartActivity(cs);
+                    }
+                }
+            });
+        }
+    }
+}
+
---
+++ PL/PL/MainActivity.cs
@@ -0,0 +1,101 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Android.Graphics;
+using Android.Content.PM;
+using Android.Support.V7.CardView;
+using System.Threading;
+using System.Collections.Generic;
+using Android.Support.V7.App;
+using ME.Itangqi.Waveloadingview;
+using static Android.Widget.SeekBar;
+using System.Threading.Tasks;
+using V7Toolbar = Android.Support.V7.Widget.Toolbar;
+using Android.Support.Design.Widget;
+using Android.Support.V4.Widget;
+using Android.Views;
+using Java;
+using static Android.Support.Design.Widget.BottomNavigationView;
+
+namespace PL
+{
+    [Activity(MainLauncher = true,Theme = "@style/Theme.DesignDemo")]
+
+    public class MainActivity : AppCompatActivity
+    {
+        DrawerLayout drawerLayout;
+        NavigationView navigationView;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+            RequestedOrientation = ScreenOrientation.Portrait;
+            //drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            // Create ActionBarDrawerToggle button and add it to the toolbar  
+            var toolbar = FindViewById<V7Toolbar>(Resource.Id.toolbar);
+            //SetSupportActionBar(toolbar);
+            var drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, Resource.String.drawer_open, Resource.String.drawer_close);
+            //drawerLayout.SetDrawerListener(drawerToggle);
+            //drawerToggle.SyncState();
+            //navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            //setupDrawerContent(navigationView); //Calling Function
+            var bottomNavigation = FindViewById<BottomNavigationView>(Resource.Id.navigation_menu);
+            bottomNavigation.NavigationItemSelected += (s, e) =>
+            {
+                switch (e.Item.ItemId)
+                {
+                    case Resource.Id.books:
+                        loadFragment(new BooksFragment());
+                        break;
+                    case Resource.Id.video:
+                        loadFragment(new VideoFragment());
+                        break;
+                    case Resource.Id.settings:
+                        loadFragment(new SettingsFragment());
+                        break;
+                }
+            };
+            loadFragment(new BooksFragment());
+        }
+        //void setupDrawerContent(NavigationView navigationView)
+        //{
+        //    navigationView.NavigationItemSelected += (sender, e) =>
+        //    {
+        //        e.MenuItem.SetChecked(true);
+        //        drawerLayout.CloseDrawers();
+        //    };
+        //}
+        //public override bool OnCreateOptionsMenu(IMenu menu)
+        //{
+        //    navigationView.InflateMenu(Resource.Menu.nav_menu); //Navigation Drawer Layout Menu Creation  
+        //    return true;
+        //}
+
+        private bool loadFragment(Fragment fragment)
+        {
+            FragmentManager fm = this.FragmentManager;
+            fm.BeginTransaction().Replace(Resource.Id.fragment_container, fragment).Commit();
+            return true;
+        }
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            Fragment fragment = null;
+                switch (item.ItemId)
+                {
+                    case Resource.Id.books:
+                        loadFragment(new BooksFragment());
+                        break;
+                    case Resource.Id.video:
+                        loadFragment(new VideoFragment());
+                        break;
+                    case Resource.Id.settings:
+                        loadFragment(new SettingsFragment());
+                        break;
+                }
+            return loadFragment(fragment);
+        }
+    }
+}
+
+
